28b4c6e905cf9952c91c68e6085ddf1394d4826f
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2020-12-16 15:58:38 +0300

    Add everything but a creation of filters

--- index-server/src/Ergvein/Index/Server/BlockchainScanning/Cypra.hs
+++ index-server/src/Ergvein/Index/Server/BlockchainScanning/Cypra.hs
@@ -1,6 +1,7 @@
 -- |
 module Ergvein.Index.Server.BlockchainScanning.Cypra where
 
+import Codec.Serialise (serialise)
 import Control.Concurrent
 import Control.Concurrent.STM
 import Control.Monad
@@ -8,11 +9,16 @@ import Control.Monad.IO.Class
 import Control.Monad.Trans.Cont
 import Control.Monad.Trans.Class
 import Control.Immortal
+import Data.Proxy
+import Data.Foldable
 import qualified Data.ByteString.Short as BSS
+import qualified Data.ByteString.Lazy  as BL
+import qualified Data.Map.Strict       as Map
 
 import Ergvein.Types.Currency
 import Ergvein.Types.Transaction
 import Ergvein.Index.Server.Monad
+import Ergvein.Index.Server.Utils
 import Ergvein.Index.Server.BlockchainScanning.Types
 
 import HSChain.Control.Channels (awaitIO)
@@ -24,8 +30,10 @@ import qualified HSChain.PoW.P2P            as PoW
 import qualified HSChain.PoW.P2P.Types      as PoW
 import qualified HSChain.PoW.Types          as PoW
 import qualified HSChain.PoW.Consensus      as PoW
+import qualified HSChain.Types.Merkle.Types as PoW
 import qualified Hschain.Utxo.Pow.App       as Cypra
 import qualified Hschain.Utxo.Pow.App.Types as Cypra
+import qualified Hschain.Utxo.Lang.Types    as Cypra
 
 scanThread :: ServerM Thread
 scanThread = create $ \_ -> do
@@ -55,19 +63,37 @@ scanThread = create $ \_ -> do
                           }
 
 
-scanBlock :: Cypra.UtxoPOWConfig t => PoW.Block (Cypra.UTXOBlock t) -> BlockInfo
+scanBlock :: forall t. Cypra.UtxoPOWConfig t => PoW.Block (Cypra.UTXOBlock t) -> BlockInfo
 scanBlock b = BlockInfo
   { blockInfoMeta       = BlockMetaInfo
     { blockMetaCurrency                = CYPRA
     , blockMetaBlockHeight             = let PoW.Height h = PoW.blockHeight b
                                          in fromIntegral h
-    , blockMetaHeaderHash              = BSS.toShort $ PoW.encodeToBS $ PoW.blockID b
-    , blockMetaPreviousHeaderBlockHash = BSS.toShort $ PoW.encodeToBS $ case PoW.prevBlock b of
+    , blockMetaHeaderHash              = encodeShort $ PoW.blockID b
+    , blockMetaPreviousHeaderBlockHash = encodeShort $ case PoW.prevBlock b of
         Nothing  -> error "scanBlock: genesis block"
         Just bid -> bid
     , blockMetaAddressFilter           = undefined
     }
-  , spentTxOutputs      = undefined
-  , blockContentTxInfos = undefined
+  , spentTxOutputs      = Map.fromList spentTxsIds
+  , blockContentTxInfos = txInfos
   }
   where
+    txInfos     = fmap (txInfo (Proxy @t)) $ toList $ Cypra.ubData $ PoW.blockData b
+    spentTxsIds = uniqueWithCount
+                $ spentInputs (Proxy @t) =<< toList (Cypra.ubData $ PoW.blockData b)
+
+txInfo :: forall t. Cypra.UtxoPOWConfig t => Proxy t -> PoW.Tx (Cypra.UTXOBlock t) -> TxInfo
+-- FIXME: Tx as type family breaks type inference. Badly!
+txInfo _ tx = TxInfo
+  { txHash         = CypTxHash $ CypTxId $ encodeShort $ PoW.txID @(Cypra.UTXOBlock t) tx
+  , txBytes        = BL.toStrict $ serialise tx
+  , txOutputsCount = fromIntegral $ length $ Cypra.tx'outputs tx
+  }
+
+spentInputs :: forall t. Cypra.UtxoPOWConfig t => Proxy t -> PoW.Tx (Cypra.UTXOBlock t) -> [TxHash]
+spentInputs = undefined
+
+
+encodeShort :: PoW.ByteRepr a => a -> BSS.ShortByteString
+encodeShort = BSS.toShort . PoW.encodeToBS
